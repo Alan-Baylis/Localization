f4e1a206cc810025276ef94a110893b24e37c8b1
Dakwamine <dakwamine@gmail.com>
2016-11-23 20:34:27 +0100

    Fixed white spaces and reformatted everything.

--- Localization.cs
+++ Localization.cs
@@ -4,135 +4,135 @@ using System.Collections.Generic;
 
 public class Localization : MonoBehaviour
 {
-	/// <summary>
-	/// The localization file.
-	/// </summary>
-	public TextAsset localizationFile;
+    /// <summary>
+    /// The localization file.
+    /// </summary>
+    public TextAsset localizationFile;
 
-	private List<Line> lines = new List<Line>();
+    private List<Line> lines = new List<Line>();
 
-	static private Localization instance;
+    static private Localization instance;
     static public Localization Instance { get { return instance; } }
 
-	void Awake()
-	{
-		DontDestroyOnLoad(gameObject);
-		instance = this;
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        instance = this;
 
 
-		// Load the localization file
-		RetrieveLocalizationFromFile();
-	}
+        // Load the localization file
+        RetrieveLocalizationFromFile();
+    }
 
 
-	/// <summary>
-	/// Gets the strings from the localization file.
-	/// </summary>
-	void RetrieveLocalizationFromFile()
-	{
-		// Do not load if reference is not set
-		if(localizationFile == null)
-		{
-			Debug.LogError("Localization:RetrieveLocalizationFromFile(): TextAsset localizationFile is not set.");
-			return;
-		}
+    /// <summary>
+    /// Gets the strings from the localization file.
+    /// </summary>
+    void RetrieveLocalizationFromFile()
+    {
+        // Do not load if reference is not set
+        if (localizationFile == null)
+        {
+            Debug.LogError("Localization:RetrieveLocalizationFromFile(): TextAsset localizationFile is not set.");
+            return;
+        }
 
 
-		// Get the rows from the TextAsset
-		string[] rows = localizationFile.text.Split(new string[] { "\r\n" }, System.StringSplitOptions.RemoveEmptyEntries);
+        // Get the rows from the TextAsset
+        string[] rows = localizationFile.text.Split(new string[] { "\r\n" }, System.StringSplitOptions.RemoveEmptyEntries);
 
 
-		// Remove the BOM character if any
-		if((int)rows[0][0] == 65279)
-		{
-			rows[0] = rows[0].TrimStart((char)65279);
-		}
+        // Remove the BOM character if any
+        if ((int)rows[0][0] == 65279)
+        {
+            rows[0] = rows[0].TrimStart((char)65279);
+        }
 
 
-		// Insert texts in the list
-		foreach(string row in rows)
-		{
-			string[] parts = row.Split('\t');
+        // Insert texts in the list
+        foreach (string row in rows)
+        {
+            string[] parts = row.Split('\t');
 
 
-			// Ignore this row if empty
-			if(parts[0].Length == 0)
-				continue;
+            // Ignore this row if empty
+            if (parts[0].Length == 0)
+                continue;
 
 
-			// Create the line (Line)
-			lines.Add(new Line(parts[0], parts[1], parts[2]));
-		}
-	}
+            // Create the line (Line)
+            lines.Add(new Line(parts[0], parts[1], parts[2]));
+        }
+    }
 
 
-	/// <summary>
+    /// <summary>
     /// Gets the localized string identified by the string parameter.
-	/// </summary>
-	/// <param name="_id">Text to retrieve id.</param>
-	/// <returns>The translated string.</returns>
-	public string GetLine(string _id)
-	{
-		Line line = lines.Find(item => item.Id == _id);
+    /// </summary>
+    /// <param name="_id">Text to retrieve id.</param>
+    /// <returns>The translated string.</returns>
+    public string GetLine(string _id)
+    {
+        Line line = lines.Find(item => item.Id == _id);
 
-		if(line == null)
-			return "Localization:GetLine() : line not found with id = " + _id.ToString();
+        if (line == null)
+            return "Localization:GetLine() : line not found with id = " + _id.ToString();
 
-		return line.GetLineText();
-	}
+        return line.GetLineText();
+    }
 
 
-	/// <summary>
+    /// <summary>
     /// Gets the Line object identified by the string parameter.
-	/// </summary>
-	/// <param name="_id">Text to retrieve id.</param>
-	/// <returns>The Line object.</returns>
-	public Line GetLineObject(string _id)
-	{
-		Line line = lines.Find(item => item.Id == _id);
+    /// </summary>
+    /// <param name="_id">Text to retrieve id.</param>
+    /// <returns>The Line object.</returns>
+    public Line GetLineObject(string _id)
+    {
+        Line line = lines.Find(item => item.Id == _id);
 
 
-		if(line == null)
-		{
-			Debug.LogError("Localization:GetLineObject() : line not found with id = " + _id.ToString());
-			return null;
-		}
+        if (line == null)
+        {
+            Debug.LogError("Localization:GetLineObject() : line not found with id = " + _id.ToString());
+            return null;
+        }
 
-		return line;
-	}
+        return line;
+    }
 
 
     /// <summary>
     /// A Line object contains an id and the translated texts.
     /// </summary>
-	public class Line
-	{
-		/// <summary>
-		/// Id de la ligne.
-		/// </summary>
-		public string Id
-		{
-			get
-			{
-				return id;
-			}
-		}
-		private string id;
+    public class Line
+    {
+        /// <summary>
+        /// Id de la ligne.
+        /// </summary>
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+        private string id;
 
 
-		/// <summary>
-		/// Available languages. Add here for more languages.
-		/// </summary>
-		public enum Language
-		{
-			FR,
-			EN
-		}
+        /// <summary>
+        /// Available languages. Add here for more languages.
+        /// </summary>
+        public enum Language
+        {
+            FR,
+            EN
+        }
 
 
-		/// <summary>
-		/// Selected language.
-		/// </summary>
+        /// <summary>
+        /// Selected language.
+        /// </summary>
         static Language currentLanguage = Language.FR;
 
 
@@ -154,10 +154,10 @@ public class Localization : MonoBehaviour
             currentLanguage = _language;
         }
 
-		/// <summary>
-		/// Text.
-		/// </summary>
-		private string french, english;
+        /// <summary>
+        /// Text.
+        /// </summary>
+        private string french, english;
 
 
         /// <summary>
@@ -166,51 +166,51 @@ public class Localization : MonoBehaviour
         /// <param name="_id">The identifier.</param>
         /// <param name="_french">French text.</param>
         /// <param name="_english">English text.</param>
-		public Line(string _id, string _french, string _english)
-		{
-			id = _id;
-			french = _french;
-			english = _english;
-		}
+        public Line(string _id, string _french, string _english)
+        {
+            id = _id;
+            french = _french;
+            english = _english;
+        }
 
 
-		/// <summary>
-		/// Gets the localized text.
-		/// </summary>
-		/// <returns>The localized text.</returns>
-		public string GetLineText()
-		{
-			switch(currentLanguage)
-			{
-				case Language.FR:
-					return french;
+        /// <summary>
+        /// Gets the localized text.
+        /// </summary>
+        /// <returns>The localized text.</returns>
+        public string GetLineText()
+        {
+            switch (currentLanguage)
+            {
+                case Language.FR:
+                    return french;
 
-				case Language.EN:
-					return english;
+                case Language.EN:
+                    return english;
 
-				default:
-					return "Localization:Line:GetLineText() : Current language undefined.";
-			}
-		}
+                default:
+                    return "Localization:Line:GetLineText() : Current language undefined.";
+            }
+        }
 
 
-		/// <summary>
-		/// Gets the localized audio file name. Use your own file naming convention.
-		/// </summary>
-		/// <returns>The localized audio file name.</returns>
-		public string GetLineAudioName()
-		{
-			switch(currentLanguage)
-			{
-				case Language.FR:
-					return id + "_FR";
+        /// <summary>
+        /// Gets the localized audio file name. Use your own file naming convention.
+        /// </summary>
+        /// <returns>The localized audio file name.</returns>
+        public string GetLineAudioName()
+        {
+            switch (currentLanguage)
+            {
+                case Language.FR:
+                    return id + "_FR";
 
-				case Language.EN:
-					return id + "_EN";
+                case Language.EN:
+                    return id + "_EN";
 
-				default:
-					return "Localization:Line:GetLineAudioName() : Current language undefined.";
-			}
-		}
-	}
+                default:
+                    return "Localization:Line:GetLineAudioName() : Current language undefined.";
+            }
+        }
+    }
 }
